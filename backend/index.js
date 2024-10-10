8538e9eddaf723d766969bab836c4e7ae6ab6621
klucky07 <khanlucky2020@gmail.com>
2024-10-10 10:41:44 +0530

    s

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors(
   {
-    origin: ['https://summary-ai-utp1.vercel.app/'], 
+    origin: ['http://summary-ai-utp1.vercel.app/'], 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     credentials: true 
   }
